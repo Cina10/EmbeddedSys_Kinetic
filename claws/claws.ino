91e00dbf94be74842cb7c096cd589b17b8cc5265
mwinitch <michaelwin26@gmail.com>
2021-03-24 22:11:30 -0400

    Updated motor code

--- claws/claws.ino
+++ claws/claws.ino
@@ -1,11 +1,47 @@
 // DC Motor
-int motorPin = 9;
+int in1Pin = 12;      // Define L293D channel 1 pin
+int in2Pin = 14;      // Define L293D channel 2 pin
+int enable1Pin = 13;  // Define L293D enable 1 pin
+int channel = 0;
+
+boolean rotationDir;  // Define a variable to save the motor's rotation direction
+int rotationSpeed;    // Define a variable to save the motor rotation speed
 
 void setup() {
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
+  // Initialize the pin into an output mode:
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+  
+  ledcSetup(channel,1000,11);         //Set PWM to 11 bits, range is 0-2047
+  ledcAttachPin(enable1Pin,channel);
 }
 
 void loop() {
-  analogWrite(motorPin, speed);
+  int potenVal = analogRead(A0);      // Convert the voltage of rotary potentiometer into digital
+  //Compare the number with value 2048, 
+  //if more than 2048, clockwise rotates, otherwise, counter clockwise rotates
+  rotationSpeed = potenVal - 2048;
+  if (potenVal > 2048)
+    rotationDir = true;
+  else
+    rotationDir = false;
+  // Calculate the motor speed
+  rotationSpeed = abs(potenVal - 2048);
+  //Control the steering and speed of the motor
+  driveMotor(rotationDir, constrain(rotationSpeed,0,2048));
+}
+
+void driveMotor(boolean dir, int spd) {
+  // Control motor rotation direction
+  if (rotationDir) {
+    digitalWrite(in1Pin, HIGH);
+    digitalWrite(in2Pin, LOW);
+  }
+  else {
+    digitalWrite(in1Pin, LOW);
+    digitalWrite(in2Pin, HIGH);
+  }
+  // Control motor rotation speed
+  ledcWrite(channel, spd);
 }
